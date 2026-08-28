c14c54f813b788e3f8b7e067775e0c7827dcc824
LuccasDev01 <luccasdeassis0304@gmail.com>
2026-08-27 21:27:16 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// Importar biblioteca express
+import express {Request, Response} from "express";
+
+// Criar a Aplicação Express 
+const app = express();
+
+// Criar a rota GET principal
+
+
+//Iniciar o servidor na porta 3000
+app.listen(8080, () => {
+    console.log("Servidor inciado na porta 8080:http://localhost:8080");
+});
