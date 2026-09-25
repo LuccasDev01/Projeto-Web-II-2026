2d747b57178fb87f039862394cef1334dc21cfef
LuccasDev01 <luccasdeassis0304@gmail.com>
2026-09-25 01:49:44 -0300

    Iniciação do CRUD

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,16 @@
+// Importar a biblioteca express;
+import express, {Request, Response} from "express";
+
+
+// Criar a Aplicação Express 
+const router = express.Router();
+
+
+// Criar a rota GET principal
+router.get("/", (req: Request, res: Response) => {
+    res.send("Bem vindo pessoal!! Tela de login na rota ");
+});
+
+//Exportar a imstrução da rota
+
+export default router;
